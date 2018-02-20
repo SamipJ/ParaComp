9a1eaafd703ca0b83e11f5a6fa848b3356d52322
SamipJ <samipjasani@gmail.com>
2018-02-21 02:28:32 +0530

    memory efficient SoE

--- SoE/parallelseive.c
+++ SoE/parallelseive.c
@@ -3,24 +3,42 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-// #include <string.h>
+#include <string.h>
 #include <time.h>
 #include <math.h>
 
+FILE* newFile(FILE* fp,int count){
+	if(fp!=NULL) fclose(fp);
+	char filename[20];
+	char countchar[4];
+	sprintf(countchar,"%d",count);
+	strcpy(filename,"output");
+	strcat(filename,countchar);
+	fp=fopen(filename,"w+");
+    if(fp==NULL){
+        printf("lol file didnt open for writing\n");
+    }
+	printf("Printing in %s\n",filename);
+	return fp;
+}
+
 int main(int argc, char** argv) {
     // Initialize the MPI environment
+    
     double t1,t2;
     MPI_Init(&argc, &argv);
     t1 = MPI_Wtime();
-    
+    FILE * fp=NULL;
     int rank, p;
+    long int global_cnt;
     int primesize;
     long int n, i, j,k;
     char* primebool;
     int* primertn;
-
+    global_cnt = 0;
     MPI_Comm_size(MPI_COMM_WORLD, &p);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Status status;
     
     
     // Print off a hello world message
@@ -28,11 +46,12 @@ int main(int argc, char** argv) {
     
     
     if(rank==0){
-        printf("Enter Number:\n");
+        printf("Enter Number in power:\n");
         scanf("%ld",&n);
         // printf("ECHO: %ld\n",n);
         // n=1000000000;
     }
+    n=pow(10,n);
     
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Bcast(&n,1,MPI_LONG,0,MPI_COMM_WORLD);
@@ -116,8 +135,15 @@ int main(int argc, char** argv) {
         size1=count;
         count1=0;
         for (i=3;i<=rtn;i+=2){
+        	
             if(primebool[i/2]=='1'){
+          //   	if(count1==1010){
+        		// 	printf("hi %ld\n",i);
+        		// }
                 primertn[count1++]=i;
+          //       if(count1==1011){
+        		// 	printf("hi %d\n",primertn[1010]);
+        		// }
             }
             if(size1==count1){
                 primertn = (int*)realloc(primertn,(count1+count)*sizeof(int));
@@ -129,12 +155,14 @@ int main(int argc, char** argv) {
         i1 = count1-count;
         MPI_Ibcast(&i1,1,MPI_INT,0,MPI_COMM_WORLD,&req);
         MPI_Ibcast(&primertn[count],i1,MPI_INT,0,MPI_COMM_WORLD,&req);
+        // printf("%d\n",primertn[1010+count] );
     }else{
         MPI_Ibcast(&primesize,1,MPI_INT,0,MPI_COMM_WORLD,&req);
         MPI_Wait(&req,MPI_STATUS_IGNORE);
         primertn = (int*)malloc(sizeof(int)*primesize);
         MPI_Ibcast(primertn,primesize,MPI_INT,0,MPI_COMM_WORLD,&req);
         MPI_Wait(&req,MPI_STATUS_IGNORE);
+        // printf("%d %d\n",primesize,primertn[1010] );
     }
 
     long int from=start,j1,k1,i1;
@@ -143,6 +171,7 @@ int main(int argc, char** argv) {
         k1 = primertn[i1];
         // printf("%d %d %d\n",k,rank,i);
         j1=from;
+        // if(k1==0) printf("hi %d %ld %ld %d\n",primesize,i1,from,rank);
         if (j1%k1!=0) j1=(from+k1-(from%k1));
         if (j1%2==0) j1=j1+k1;
         if (j1<k1*k1) j1= k1*k1;
@@ -153,7 +182,6 @@ int main(int argc, char** argv) {
             primebool[(j1-start)/2]='0';
         }
     }
-
 
 
     long int* primenumbers = (long int*)malloc(10*sizeof(long int));
@@ -169,6 +197,8 @@ int main(int argc, char** argv) {
             }
         }
     }
+    free(primebool);
+    free(primertn);
 
     // MPI_Barrier(MPI_COMM_WORLD);
 
@@ -181,40 +211,67 @@ int main(int argc, char** argv) {
     // printf("Hello World from %d\n",rank);
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Gather(&count, 1, MPI_INT,countarr,1, MPI_INT,0, MPI_COMM_WORLD);
-    int *displs = NULL;
+    // int *displs = NULL;
     if(rank==0){
-        displs=(int*)malloc(p*sizeof(int));
-        displs[0]=0;
+        // displs=(int*)malloc(p*sizeof(int));
+        finalcount=0;
         i1=1;
         while(i1<p){
             finalcount+=countarr[i1-1];
-            displs[i1]=finalcount;
+            // displs[i1]=finalcount;
             i1++;
         }
         finalcount+=countarr[i1-1]+1;
-        finalprimes=(long int*)malloc(finalcount*sizeof(long int));
-        finalprimes[0]=2;
+        // finalprimes=(long int*)malloc(finalcount*sizeof(long int));
+        // finalprimes[0]=2;
 
     }
     // printf("Hello World from %d\n",rank);
-    MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Gatherv(primenumbers, count, MPI_LONG,&finalprimes[1], countarr, displs, MPI_LONG,0, MPI_COMM_WORLD);
-
-    if (rank==0)  printf("Total %d\n",finalcount);
-
-   FILE * fp;
-   fp = fopen ("output", "w+");
-
-    if(rank==0){
-        i=0;
-        while(i<finalcount){
-            fprintf(fp,"%ld\n",finalprimes[i] );
-            i++;
+    // MPI_Barrier(MPI_COMM_WORLD);
+    if (rank!=0){
+        MPI_Send(primenumbers, count, MPI_LONG,0,0, MPI_COMM_WORLD);
+        free(primenumbers);
+    }
+    else{
+        printf("Total %d\n",finalcount);
+        if(global_cnt%100000000==0) {
+                fp=newFile(fp,global_cnt/100000000);
+                }       
+        fprintf(fp,"2\n");
+        global_cnt++;
+        for (i=0;i<countarr[0];i++){
+            if(global_cnt%100000000==0) {
+                fp=newFile(fp,global_cnt/100000000);
+                }       
+            fprintf(fp,"%ld\n",primenumbers[i]);
+            global_cnt++;
         }
+        free(primenumbers);
+        for(j=1;j<p;j++){
+            primenumbers = (long int*)calloc(countarr[j],sizeof(long int));
+            MPI_Recv(primenumbers,countarr[j],MPI_LONG,j,0,MPI_COMM_WORLD,&status);
+            for (i=0;i<countarr[j];i++){
+                if(global_cnt%100000000==0){
+                fp=newFile(fp,global_cnt/100000000);        
+                }
+                fprintf(fp,"%ld\n",primenumbers[i]);
+                global_cnt++;
+            }
+            free(primenumbers);
+        }
+        fclose(fp);
+        
+    // if(rank==0){
+    //     i=0;
+    //     while(i<finalcount){
+    //         fprintf(fp,"%ld\n",finalprimes[i] );
+    //         i++;
+    //     }
     }
 
 
-   // fclose(fp);
+    
+    
     // MPI_Ibcast(&n,1,MPI_INT,0,MPI_COMM_WORLD,&req);
     // MPI_Wait(&req,MPI_STATUS_IGNORE);
 
